51e973aeb9e909f0c2e97a6cc5d2256a1ce9ed3a
Vlad Fedorov <79852880+Vlad19982602@users.noreply.github.com>
2024-04-09 11:42:04 +0500

    added counter

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ClassCounter from './components/UI/ClassCounter';
 import MyButton from './components/UI/button/MyButton.js';
 
 function App() {
@@ -7,7 +8,9 @@ function App() {
       
       <MyButton />
       <hr></hr>
-      
+
+      <ClassCounter/>
+      <hr></hr>
       <MyButton />
     </div>
   );
